d52ebe85413bfd6c5dec08e889b3d3dae0c08b97
nguyendamnhatanh <anhndn145@gmail.com>
2022-05-25 22:06:08 +0700

    add label for menu form
    add name + id

--- NT106.M21.1-Lab05-Nhom02-20521742/Menu.Designer.cs
+++ NT106.M21.1-Lab05-Nhom02-20521742/Menu.Designer.cs
@@ -31,13 +31,18 @@
             this.btnSendMail = new System.Windows.Forms.Button();
             this.btnRecMail = new System.Windows.Forms.Button();
             this.btnAppMail = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnSendMail
             // 
-            this.btnSendMail.Location = new System.Drawing.Point(38, 38);
+            this.btnSendMail.Location = new System.Drawing.Point(51, 47);
+            this.btnSendMail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSendMail.Name = "btnSendMail";
-            this.btnSendMail.Size = new System.Drawing.Size(126, 81);
+            this.btnSendMail.Size = new System.Drawing.Size(168, 100);
             this.btnSendMail.TabIndex = 0;
             this.btnSendMail.Text = "Send Mail";
             this.btnSendMail.UseVisualStyleBackColor = true;
@@ -45,9 +50,10 @@
             // 
             // btnRecMail
             // 
-            this.btnRecMail.Location = new System.Drawing.Point(38, 144);
+            this.btnRecMail.Location = new System.Drawing.Point(51, 177);
+            this.btnRecMail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnRecMail.Name = "btnRecMail";
-            this.btnRecMail.Size = new System.Drawing.Size(126, 81);
+            this.btnRecMail.Size = new System.Drawing.Size(168, 100);
             this.btnRecMail.TabIndex = 0;
             this.btnRecMail.Text = "Recieve Mail";
             this.btnRecMail.UseVisualStyleBackColor = true;
@@ -55,26 +61,69 @@
             // 
             // btnAppMail
             // 
-            this.btnAppMail.Location = new System.Drawing.Point(38, 253);
+            this.btnAppMail.Location = new System.Drawing.Point(51, 311);
+            this.btnAppMail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAppMail.Name = "btnAppMail";
-            this.btnAppMail.Size = new System.Drawing.Size(126, 81);
+            this.btnAppMail.Size = new System.Drawing.Size(168, 100);
             this.btnAppMail.TabIndex = 0;
             this.btnAppMail.Text = "App Mail";
             this.btnAppMail.UseVisualStyleBackColor = true;
             this.btnAppMail.Click += new System.EventHandler(this.btnAppMail_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(474, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Khưu Minh Phong – 20521742";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(474, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nguyễn Đàm Nhật Anh – 20520880";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(474, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Phan Nguyễn Văn Duy – 20520168";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(474, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Vi Minh Tiến – 20520810";
+            // 
             // Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(203, 381);
+            this.ClientSize = new System.Drawing.Size(893, 500);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnAppMail);
             this.Controls.Add(this.btnRecMail);
             this.Controls.Add(this.btnSendMail);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Menu";
             this.Text = "Menu";
             this.Load += new System.EventHandler(this.Menu_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,5 +132,9 @@
         private System.Windows.Forms.Button btnSendMail;
         private System.Windows.Forms.Button btnRecMail;
         private System.Windows.Forms.Button btnAppMail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
